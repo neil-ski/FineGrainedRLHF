9d862ea97fd3ea133d7180b03fe4df1ca29e9e47
nflu <nflugovoy@berkeley.edu>
2026-03-19 13:01:19 -0700

    WIP

--- tasks/qa_feedback/training/train_finegrained.py
+++ tasks/qa_feedback/training/train_finegrained.py
@@ -29,6 +29,9 @@ from fgrlhf.utils import ensure_dir, set_seed, reduce_mean, reduce_sum, ceil_div
 
 from reward import FineGrainedReward
 
+print(datasets.__version__)
+raise Exception("panic")
+
 logging.basicConfig(level=logging.ERROR)
 
 # prepare accelerator and logger
