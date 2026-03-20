60d2d118a9521b8a01a6f43644d995d87dfc7198
nflu <nflugovoy@berkeley.edu>
2026-03-19 18:32:31 -0700

    WIP

---
+++ tasks/qa_feedback/training/train_mdr.sh
@@ -0,0 +1,6 @@
+accelerate launch \
+    --num_machines 1 \
+    --machine_rank 0 \
+    --num_processes 1 \
+    --mixed_precision bf16 \
+    tasks/qa_feedback/training/train_finegrained.py --config tasks/qa_feedback/training/mdr_config.yml
